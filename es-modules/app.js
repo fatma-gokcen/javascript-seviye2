33b4b38be43ad119046340af6c466ce9a5589d3e
fatmagokcen-software <fatmagokcen53@gmail.com>
2025-12-01 16:23:15 +0300

    math.js ve app.js dosyalarına eklemeler yapıldı

---
+++ es-modules/app.js
@@ -0,0 +1,6 @@
+import greet, { sum, multiply } from './math.js';
+
+console.log(sum(3, 4));       // 7
+console.log(multiply(5, 6));  // 30
+
+greet("Fatma");                // Hello, Fatma!
---
+++ es-modules/math.js
@@ -0,0 +1,8 @@
+// Named export
+export const sum = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
+
+// Default export
+export default function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
